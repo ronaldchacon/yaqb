1cdd68118c7ef954193f82bf9691f61f1e2d5490
Ronald Chacon <ronald.chacon87@gmail.com>
2019-10-05 15:58:43 -0600

    Add configuration
    - dynamically detect kaminari
    - create setter for future implementations to manually override

---
+++ lib/yaqb/configuration.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+module Yaqb
+  class << self
+    def configure
+      yield(configuration)
+    end
+
+    def configuration
+      @configuration ||= Configuration.new
+    end
+    alias config configuration
+  end
+
+  class Configuration
+    def paginator
+      @paginator || set_paginator
+    end
+
+    def paginator=(paginator)
+      case paginator.to_sym
+      when :kaminari
+        @paginator = :kaminari
+      else
+        raise StandardError, paginator_error_message(paginator)
+      end
+    end
+
+    private
+
+    def set_paginator
+      @paginator = :kaminari if defined?(Kaminari)
+    end
+
+    def paginator_error_message(paginator)
+      <<~HEREDOC.chomp
+        Invalid Paginator: (#{paginator}). Currently supported paginators are: [Kaminari].
+      HEREDOC
+    end
+  end
+end
