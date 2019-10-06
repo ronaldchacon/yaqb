eda4eb6dedfd06bd5dec5ed054d298a993b2ff77
Ronald Chacon <ronald.chacon87@gmail.com>
2019-10-06 02:07:30 -0600

    call orchestrator and remove unnesccessary requires

--- lib/yaqb/query_builder.rb
+++ lib/yaqb/query_builder.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
-require 'yaqb/query_builders/paginate'
-require 'yaqb/query_builders/sort'
-require 'yaqb/query_builders/filter'
 require 'yaqb/query_builders/query_orchestrator'
 
 module Yaqb
   module QueryBuilder
     private
 
-    def orchestrate; end
+    def orchestrate(scope)
+      Yaqb::QueryBuilders::QueryOrchestrator.new(scope, params, request, response).call
+    end
   end
 end
