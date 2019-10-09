edd0c8b0bcc81558c2e2729820e1d703c64b683d
Ronald Chacon <ronald.chacon87@gmail.com>
2019-10-08 18:51:49 -0600

    Update filter class

--- lib/yaqb/query_builders/filter.rb
+++ lib/yaqb/query_builders/filter.rb
@@ -3,12 +3,84 @@
 module Yaqb
   module QueryBuilders
     class Filter
-      def initialize(scope, query_params)
+      PREDICATES = %w[eq cont notcont start end gt lt].freeze
+
+      def initialize(scope, params, options = {})
         @scope = scope
-        @query_params = query_params
+        @filters = params['q'] || {}
+        @options = options
       end
 
       def filter
+        return @scope unless @filters.any?
+
+        @filters = format_filters
+        validate_filters
+        build_filter_scope
+
+        @scope
+      end
+
+      private
+
+      def format_filters
+        @filters.each_with_object({}) do |(key, value), hash|
+          hash[key] = {
+            value: value,
+            column: key.split('_')[0...-1].join('_'),
+            predicate: key.split('_').last
+          }
+        end
+      end
+
+      def build_filter_scope
+        @filters.each do |_key, data|
+          @scope = send(data[:predicate], data[:column], data[:value])
+        end
+      end
+
+      def eq(column, value)
+        @scope.where(column => value)
+      end
+
+      def cont(column, value)
+        @scope.where("#{column} LIKE ?", "%#{value}%")
+      end
+
+      def notcont(column, value)
+        @scope.where("#{column} NOT LIKE ?", "%#{value}%")
+      end
+
+      def start(column, value)
+        @scope.where("#{column} LIKE ?", "#{value}%")
+      end
+
+      def end(column, value)
+        @scope.where("#{column} LIKE ?", "%#{value}")
+      end
+
+      def gt(column, value)
+        @scope.where("#{column} > ?", value)
+      end
+
+      def lt(column, value)
+        @scope.where("#{column} < ?", value)
+      end
+
+      def validate_filters
+        attributes = @options[:columns]
+        @filters.each do |key, data|
+          error!(key, data) unless attributes.include?(data[:column])
+          error!(key, data) unless PREDICATES.include?(data[:predicate])
+        end
+      end
+
+      def error!(key, data)
+        columns = @options[:columns].join(', ')
+        pred = PREDICATES.join(',')
+
+        raise QueryBuilderError.new("q[#{key}]=#{data[:value]}"),
+              "Invalid Filter params. Allowed columns: (#{columns}), 'predicates': #{pred}"
       end
     end
   end
