c5e1ed9850645e43a3b416bc2fab3017f33f4c4c
Ronald Chacon <ronald.chacon87@gmail.com>
2019-10-10 21:38:07 -0600

    do not not remove `\n` from warning

--- lib/yaqb/hooks.rb
+++ lib/yaqb/hooks.rb
@@ -3,7 +3,7 @@
 begin; require 'kaminari'; rescue LoadError; end
 
 unless defined?(Kaminari)
-  Kernel.warn <<~HEREDOC.chomp
+  Kernel.warn <<~HEREDOC
     Warning: Yaqb relies on Kaminari. Please
     install dependency by adding the following to your Gemfile:
     gem 'kaminari'
