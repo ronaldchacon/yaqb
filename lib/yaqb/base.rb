ea101f812264423a511c623f639a817a86051da2
Ronald Chacon <ronald.chacon87@gmail.com>
2019-10-10 21:35:16 -0600

    Rescue from query builder error automatically

--- lib/yaqb/base.rb
+++ lib/yaqb/base.rb
@@ -4,10 +4,26 @@ require 'yaqb/query_builders/query_orchestrator'
 
 module Yaqb
   module Base
+    def self.included(base)
+      base.extend ClassMethods
+      base.class_eval do
+        rescue_from Yaqb::Errors::QueryBuilderError, with: :builder_error
+      end
+    end
+
     private
 
     def orchestrate(scope, presenter)
       QueryBuilders::QueryOrchestrator.new(scope, params, request, response, presenter).call
+    end
+
+    def builder_error(error)
+      render status: 400, json: {
+        error: {
+          message: error.message,
+          invalid_params: error.invalid_params
+        }
+      }
     end
   end
 end
