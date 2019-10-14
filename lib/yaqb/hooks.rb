50fbc0b2e47fee31c78e0d731319f7db4c28a3ea
Ronald Chacon <ronald.chacon87@gmail.com>
2019-10-13 23:58:53 -0600

    Add will_paginate to hooks

--- lib/yaqb/hooks.rb
+++ lib/yaqb/hooks.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 begin; require 'kaminari'; rescue LoadError; end
+begin; require 'will_paginate'; rescue LoadError; end
 
-unless defined?(Kaminari)
+unless defined?(Kaminari) || defined?(WillPaginate::CollectionMethods)
   Kernel.warn <<~HEREDOC
-    Warning: Yaqb relies on Kaminari. Please
-    install dependency by adding the following to your Gemfile:
-    gem 'kaminari'\n
+    Warning: Yaqb relies on Kaminari or WillPaginate. Please
+    install dependency by adding one of the following to your Gemfile:
+    gem 'kaminari'
+    gem 'will_paginate'\n
   HEREDOC
 end
