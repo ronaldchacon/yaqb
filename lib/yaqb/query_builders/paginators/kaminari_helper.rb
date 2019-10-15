3ca2bb88e706320b411e2d410eb832f7e441b057
Ronald Chacon <ronald.chacon87@gmail.com>
2019-10-14 21:03:27 -0600

    move kaminar helper methods to kaminari_helper

--- lib/yaqb/query_builders/paginators/kaminari_helper.rb
+++ lib/yaqb/query_builders/paginators/kaminari_helper.rb
@@ -9,6 +9,35 @@ module Yaqb
         def paginate!(scope)
           scope.page(@page).per(@per)
         end
+
+        def show_first_link?
+          @scope.total_pages > 1 && !@scope.first_page?
+        end
+
+        def show_previous_link?
+          !@scope.first_page?
+        end
+
+        def show_next_link?
+          !@scope.last_page?
+        end
+
+        def show_last_link?
+          @scope.total_pages > 1 && !@scope.last_page?
+        end
+
+        def pages
+          @pages ||= {}.tap do |h|
+            h[:first] = 1 if show_first_link?
+            h[:prev] = @scope.current_page - 1 if show_previous_link?
+            h[:next] = @scope.current_page + 1 if show_next_link?
+            h[:last] = @scope.total_pages if show_last_link?
+          end
+        end
+
+        def default_per_page
+          Kaminari.config.default_per_page
+        end
       end
     end
   end
