2b64c9c685aebc27282134f7edd9b123189a0ee7
Ronald Chacon <ronald.chacon87@gmail.com>
2019-10-14 21:04:50 -0600

    dynamically include paginator helper

--- lib/yaqb/query_builders/paginate.rb
+++ lib/yaqb/query_builders/paginate.rb
@@ -1,13 +1,17 @@
 # frozen_string_literal: true
 
+require "yaqb/query_builders/paginators/#{Yaqb.config.paginator}_helper"
+
 module Yaqb
   module QueryBuilders
     class Paginate
+      include "Yaqb::QueryBuilders::Paginators::#{Yaqb.config.paginator.to_s.classify}Helper".constantize
+
       def initialize(scope, query_params, url)
         @query_params = query_params
         @page = validate_param!(:page, 1)
-        @per = validate_param!(:per, Kaminari.config.default_per_page)
-        @scope = scope.page(@page).per(@per)
+        @per = validate_param!(:per, default_per_page)
+        @scope = paginate!(scope)
         @url = url
       end
 
@@ -24,37 +28,12 @@ module Yaqb
 
       private
 
-      def show_first_link?
-        @scope.total_pages > 1 && !@scope.first_page?
-      end
-
-      def show_previous_link?
-        !@scope.first_page?
-      end
-
-      def show_next_link?
-        !@scope.last_page?
-      end
-
-      def show_last_link?
-        @scope.total_pages > 1 && !@scope.last_page?
-      end
-
-      def pages
-        @pages ||= {}.tap do |h|
-          h[:first] = 1 if show_first_link?
-          h[:prev] = @scope.current_page - 1 if show_previous_link?
-          h[:next] = @scope.current_page + 1 if show_next_link?
-          h[:last] = @scope.total_pages if show_last_link?
-        end
-      end
-
       def validate_param!(name, default)
         return default unless @query_params[name]
 
         unless @query_params[name] =~ /\A\d+\z/
           raise Errors::QueryBuilderError.new("#{name}=#{@query_params[name]}"),
-          'Invalid Pagination params. Only numbers are supported for "page" and "per"'
+          'Invalid pagination params. Only numbers are supported for "page" and "per"'
         end
 
         @query_params[name]
