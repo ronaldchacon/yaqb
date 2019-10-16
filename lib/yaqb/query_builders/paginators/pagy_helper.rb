aa9d2e4dfb4ceca44485ccff07175d3c55ee5613
Ronald Chacon <ronald.chacon87@gmail.com>
2019-10-15 20:22:04 -0600

    Add pagy_helper skeleton

---
+++ lib/yaqb/query_builders/paginators/pagy_helper.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Yaqb
+  module QueryBuilders
+    module Paginators
+      module PagyHelper
+        private
+
+        def paginate!(scope)
+        end
+
+        def show_first_link?
+        end
+
+        def show_previous_link?
+        end
+
+        def show_next_link?
+        end
+
+        def show_last_link?
+        end
+
+        def pages
+        end
+
+        def default_per_page
+        end
+      end
+    end
+  end
+end
