5160ef162ebd2414dbfa0d30524455e715caec67
Ronald Chacon <ronald.chacon87@gmail.com>
2019-10-15 20:10:25 -0600

    Add raise/rescue to hooks

--- lib/yaqb/hooks.rb
+++ lib/yaqb/hooks.rb
@@ -11,3 +11,17 @@ unless defined?(Kaminari) || defined?(WillPaginate::CollectionMethods)
     gem 'will_paginate'\n
   HEREDOC
 end
+
+begin
+  if defined?(Kaminari)
+    require 'yaqb/query_builders/paginators/kaminari_helper'
+    Yaqb::QueryBuilders::Paginate.send(:include, Yaqb::QueryBuilders::Paginators::KaminariHelper)
+  elsif defined?(WillPaginate::CollectionMethods)
+    require 'yaqb/query_builders/paginators/will_paginate_helper'
+    Yaqb::QueryBuilders::Paginate.send(:include, Yaqb::QueryBuilders::Paginators::WillPaginateHelper)
+  else
+    raise LoadError
+  end
+rescue LoadError
+  exit
+end
