a90320922fa33af92aeb538d6922fb70b8eb2056
Ronald Chacon <ronald.chacon87@gmail.com>
2019-10-15 20:19:10 -0600

    Add Pagy to hooks

--- lib/yaqb/hooks.rb
+++ lib/yaqb/hooks.rb
@@ -2,13 +2,15 @@
 
 begin; require 'kaminari'; rescue LoadError; end
 begin; require 'will_paginate'; rescue LoadError; end
+begin; require 'pagy'; rescue LoadError; end
 
-unless defined?(Kaminari) || defined?(WillPaginate::CollectionMethods)
+unless defined?(Kaminari) || defined?(WillPaginate::CollectionMethods) || defined?(Pagy)
   Kernel.warn <<~HEREDOC
-    Warning: Yaqb relies on Kaminari or WillPaginate. Please
+    Warning: Yaqb relies on Kaminari or WillPaginate or Pagy. Please
     install dependency by adding one of the following to your Gemfile:
     gem 'kaminari'
-    gem 'will_paginate'\n
+    gem 'will_paginate'
+    gem 'pagy'\n
   HEREDOC
 end
 
@@ -19,6 +21,9 @@ begin
   elsif defined?(WillPaginate::CollectionMethods)
     require 'yaqb/query_builders/paginators/will_paginate_helper'
     Yaqb::QueryBuilders::Paginate.send(:include, Yaqb::QueryBuilders::Paginators::WillPaginateHelper)
+  elsif defined?(Pagy)
+    require 'yaqb/query_builders/paginators/pagy_helper'
+    Yaqb::QueryBuilders::Paginate.send(:include, Yaqb::QueryBuilders::Paginators::PagyHelper)
   else
     raise LoadError
   end
