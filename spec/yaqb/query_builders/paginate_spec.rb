c31ffc5064a6bd153e36df156086886058163e95
Ronald Chacon <ronald.chacon87@gmail.com>
2019-10-23 23:04:33 -0600

    Add specs for paginate query builder

---
+++ spec/yaqb/query_builders/paginate_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+RSpec.describe Yaqb::QueryBuilders::Paginate do
+  let(:recipe1) { create(:recipe) }
+  let(:recipe2) { create(:recipe) }
+  let(:recipe3) { create(:recipe) }
+  let(:recipes) { [recipe1, recipe2, recipe3] }
+  let(:scope) { Recipe.all }
+  let(:params) { HashWithIndifferentAccess.new(page: '1', per: '2') }
+  let(:url) { 'http://www.example.com/v1/recipes' }
+  let(:paginate) { Yaqb::QueryBuilders::Paginate.new(scope, params, url) }
+  let(:paginated) { paginate.paginate }
+
+  before { recipes }
+
+  describe '#paginate' do
+    it { expect(paginated.size).to eq 2 }
+    it { expect(paginated.first).to eq recipe1 }
+    it { expect(paginated.last).to eq recipe2 }
+  end
+
+  describe '#links' do
+    let(:links) { paginate.links }
+
+    context 'when first page' do
+      it { expect(links).to include(next: "#{url}?page=2&per=2") }
+      it { expect(links).to include(last: "#{url}?page=2&per=2") }
+    end
+
+    context 'when last page' do
+      let(:params) { HashWithIndifferentAccess.new(page: '2', per: '2') }
+      it { expect(links).to include(first: "#{url}?page=1&per=2") }
+      it { expect(links).to include(prev: "#{url}?page=1&per=2") }
+    end
+  end
+end
